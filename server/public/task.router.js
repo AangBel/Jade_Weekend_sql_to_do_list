5dee4a1bd8a381a9a1eb2419016a77162a4fbb4e
Angelica Beltran <angelica.beltran11@gmail.com>
2023-09-01 15:26:37 -0500

    created table and doing server side router basic

---
+++ server/public/task.router.js
@@ -0,0 +1,69 @@
+const express = require('express');
+const router = express.Router();
+
+// DB CONNECTION
+// TODO://
+//check the pool .js mapping!
+const pool = require('../pool.js');
+
+console.log('we are inside tasks router');
+
+// GET
+router.get('/', (req, res) => {
+console.log('we are inside the get request')
+    let queryText = 'SELECT * FROM "tasks";'
+
+    pool.query(queryText).then(
+        (result) => {
+            res.send(result.rows)
+        }
+    ).catch(
+        (err) => {
+            console.log(`Error making query ${queryText}`, err);
+            res.sendStatus(500);
+        }
+    )
+});
+
+// POST
+
+router.post('/', (req, res) => {
+    // Do this with a DB instead
+    console.log('we are inside the post ')
+    //is it ok that this is the same name then in the client i think or the server?
+    let newTaskToAddToDatabase = req.body
+
+    console.log("res.body:", req.body);
+
+    const queryText = `
+    INSERT INTO tasks (task)
+    VALUES ($1)
+    `
+
+    console.log('The query we\'re sending to postgres:', queryText);
+
+    // Then, find a way to pass it to our pool (aka, the connection to the db).
+    // Then it's pg's problem
+
+    pool.query(queryText, [newTaskToAddToDatabase.task])
+        .then(
+            (result) => {
+                res.sendStatus(200);
+            }
+        ).catch(
+            (err) => {
+                console.log(`Error making query ${queryText}`, err);
+                res.sendStatus(500);
+            }
+        )
+
+});
+
+// PUT
+
+
+// DELETE
+
+
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -2,7 +2,26 @@ const express = require('express');
 const app = express();
 app.use(express.static('server/public'));
 // Routes go here
+
+
+app.post('/tasks', (req, res)=>{
+console.log('in app.post in the server.js!');
+
+// TODO
+//this is coming out as undefined!!!! come back to this after group!!!!
+let newTaskToAddToDatabase = req.body;
+console.log(newTaskToAddToDatabase);
+
+
+})
+
+
+
+
+
+
 let port = 5001;
 app.listen(5001, function () {
 console.log(`You started the server! It is running on ${port} .`);
 })
+
