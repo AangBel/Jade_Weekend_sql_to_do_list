656a38ec9cd3f63d6085636623fbac535a6cb1b7
Angelica Beltran <angelica.beltran11@gmail.com>
2023-09-01 14:53:00 -0500

    under new file attempt

--- server/public/client.js
+++ server/public/client.js
@@ -1,4 +1,57 @@
 $(document).ready(onReady);
 function onReady() {
-console.log("Client side javascript works!");
-}
+  console.log("Client side javascript works!");
+  $("#add_btn").on("click", addButtonFunction);
+} //end of onready
+
+function addButtonFunction() {
+  let taskInput = $("#taskText").val();
+  console.log("this should be the task input value:", taskInput);
+
+  let taskToSend = {
+    task: taskInput,
+  };
+  console.log("this should be the task to send", taskToSend);
+
+  $.ajax({
+    method: "POST",
+    url: "/tasks",
+    data: taskToSend,
+  })
+    .then(function (response) {
+      console.log(response);
+      //    TODO;
+      //set up in here the function to refresh the list- the get
+    })
+    .catch(function (error) {
+      console.log("error in tasks post under add button function", error);
+      alert("Error. Please try again later.");
+    });
+} //end of addButtonFunction
+
+function getTasks() {
+  console.log("in getTasks");
+  //TODO
+
+  // $("#viewKoalas").empty();
+
+  // ajax call to server to get koalas
+  $.ajax({
+    method: "GET",
+    url: "/tasks",
+  })
+    .then(function (response) {
+      const tasksToReceive = response;
+      console.log("retrieved data", tasksToReceive);
+      for (let tasks of tasksToReceive) {
+        $("#taskDiv").append(`
+                  <tr>
+                      <td>${tasks.task}</td>
+                  </tr>
+                  `);
+      }
+    })
+    .catch(function (error) {
+      console.log("error in tasks get in get tasks function", error);
+    });
+} // end getKoalas
