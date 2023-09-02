b3a7c1f40173c41fa175a6699e538a6962245cbf
Angelica Beltran <angelica.beltran11@gmail.com>
2023-09-01 19:53:23 -0500

    working on aesthetic

--- server/public/client.js
+++ server/public/client.js
@@ -9,7 +9,7 @@ function addButtonFunction() {
   console.log("this should be the task input value:", taskInput);
 
   let taskToSend = {
-    task: taskInput
+    task: taskInput,
   };
   console.log("this should be the task to send", taskToSend);
 
@@ -20,19 +20,19 @@ function addButtonFunction() {
   })
     .then(function (response) {
       console.log(response);
-    getTasks();
+      getTasks();
     })
     .catch(function (error) {
       console.log("error in tasks post under add button function", error);
-    //   alert("Error. Please try again later.");
+      //   alert("Error. Please try again later.");
     });
 } //end of addButtonFunction
 
 function getTasks() {
   console.log("in getTasks");
-  //TODO
 
-  // $("#viewKoalas").empty();
+  //TODO
+  // $("#taskDiv").empty();
 
   $.ajax({
     method: "GET",
@@ -41,11 +41,16 @@ function getTasks() {
     .then(function (response) {
       const tasksToReceive = response;
       console.log("retrieved data", tasksToReceive);
+      const taskTableBody = $("#taskDiv tbody");
+      taskTableBody.empty();
+
       for (let tasks of tasksToReceive) {
         $("#taskDiv").append(`
                   <tr>
-                      <td>${tasks.task}</td>
+                      <td>${tasks.task}</td><td><button>✅</button></td>
                   </tr>
+                 
+                
                   `);
       }
     })
